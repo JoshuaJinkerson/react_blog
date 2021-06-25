0157504056fc91aca58cf888b425c68096d13b07
JoshJ <JoshuaJinkerson@gmail.com>
2021-06-25 10:52:45 -0700

    Update CSS and config initial states

--- src/Component/newBlogPost/newBlogComponent.js
+++ src/Component/newBlogPost/newBlogComponent.js
@@ -2,11 +2,50 @@ import React from 'react';
 import './newBlogPostStyle.css'
 
 export class BlogComment extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            title:'',
+            post:''
+        }
+
+        this.handleTitleChange =this.handleTitleChange.bind(this)
+        this.handlePostChange =this.handlePostChange.bind(this)
+
+    }
+
+    handlePostChange(e){
+        this.setState({
+            post: e.target.value
+        })    
+    }
+
+    handleTitleChange(e){
+        this.setState({
+            title: e.target.value,
+        })    
+    }
+
+
     render(){
         return (
             <div className="newPost">
-                <textarea></textarea>
+                <form>
+                <input 
+                    type="text" 
+                    placeholder="Blog Title"
+                    title={this.state.value}
+                    onChange={this.handleTitleChange}
+                    />
+                <textarea 
+                    placeholder="Blog Post"
+                    post={this.state.value}
+                    onChange={this.handlePostChange}
+                >
+                </textarea>
+                <br/>
                 <button type='submit'>Submit</button>
+                </form>
             </div>
         )
     }
